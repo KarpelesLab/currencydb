45c49f1456f543ba1510e6920facf7b8bbfc3e65
Mark Karpeles <magicaltux@gmail.com>
2023-05-02 22:47:57 +0900

    add inr

---
+++ c-inr.go
@@ -0,0 +1,10 @@
+package currencydb
+
+var INR = &Currency{
+	ISO:            "INR",
+	Country:        "IN",
+	Name:           "Ruppee",
+	Symbol:         "INR",
+	Decimals:       2,
+	SymbolPosition: After,
+}
