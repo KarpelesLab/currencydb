459e0ab5f8a2047899797679e21bc5b18329e5c2
Mark Karpeles <magicaltux@gmail.com>
2023-06-06 20:30:52 +0900

    add countries for DKK

--- c-dkk.go
+++ c-dkk.go
@@ -3,6 +3,7 @@ package currencydb
 var DKK = &Currency{
 	ISO:            "DKK",
 	Country:        "DK",
+	Countries:      []string{"DK", "FO", "GL"},
 	Name:           "Danish kroner",
 	Symbol:         "Kr",
 	Decimals:       2,
