3048bdbf967399d77c8f937f30c6a4066d28c8f9
Mark Karpeles <magicaltux@gmail.com>
2023-06-06 20:33:45 +0900

    add NOK countries

--- c-nok.go
+++ c-nok.go
@@ -3,6 +3,7 @@ package currencydb
 var NOK = &Currency{
 	ISO:            "NOK",
 	Country:        "NO",
+	Countries:      []string{"BV", "NO", "SJ"},
 	Name:           "Norwegian krone",
 	Symbol:         "Kr",
 	Decimals:       2,
