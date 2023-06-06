1478ed2d72f1ea2697de6189116faeb18816cb67
Mark Karpeles <magicaltux@gmail.com>
2023-06-06 23:06:13 +0900

    add comment for XSU

--- c-xsu.go
+++ c-xsu.go
@@ -1,10 +1,12 @@
 package currencydb
 
+// https://en.wikipedia.org/wiki/SUCRE
+
 var XSU = &Currency{
 	ISO:            "XSU",
 	Type:           Other,
 	Countries:      []string{"BO", "CU", "EC", "NI", "VE"},
-	Name:           "SUCRE (UNIT OF ACCOUNT)",
+	Name:           "SUCRE (unit of account)",
 	Symbol:         "XSU",
 	Decimals:       0,
 	SymbolPosition: After,
